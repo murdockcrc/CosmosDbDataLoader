1bfd45ece7aee355b296c620a91df3795cf3290d
Luis Delgado <luis.delgado@codit.swiss>
2017-05-29 17:12:39 +0200

    first commit

---
+++ PremiumTableGetStarted/Program.cs
@@ -0,0 +1,236 @@
+﻿namespace TableSBS
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Diagnostics;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Table;
+    using System.Linq;
+    using System.IO;
+    using System.Threading.Tasks;
+    using System.Collections.Concurrent;
+    using Microsoft.VisualBasic;
+    using Microsoft.VisualBasic.FileIO;
+
+    /// <summary>
+    /// This sample program shows how to use the Azure storage SDK to work with premium tables (created using the Azure Cosmos DB service)
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Run common Table CRUD and query operations using the Azure Cosmos DB endpoints ("premium tables")
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
+        public static void Main(string[] args)
+        {
+            string folderPath = null;
+            string connectionString = ConfigurationManager.AppSettings["PremiumStorageConnectionString"];
+
+            if (args.Length >= 0)
+            {
+               folderPath = args[0];
+            }
+
+            if (folderPath == null)
+            {
+                throw new Exception("Need to provide a folder path");
+            }
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            Program p = new Program();
+
+            Task.Run(async () =>
+            {
+                await p.Run(tableClient, folderPath);
+            }).GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Retrieves all files in the supplied path. The program expects this folder to contains only CSV files, all with the same schema
+        /// </summary>
+        /// <param name="folderPath">Absolute path to the folder where CSV files are stored</param>
+        /// <returns></returns>
+        private string[] GetFiles(string folderPath)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Retrieving files to import");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            return Directory.GetFiles(folderPath);
+        }
+
+        /// <summary>
+        /// Reads the CSV and returns a List with all rows converted into an instance of ITableEntity
+        /// </summary>
+        /// <param name="filePath">Absolute path of the file to read</param>
+        /// <returns></returns>
+        private List<ITableEntity> ReadCsvFile(string filePath)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Retrieving entities to insert");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            var entitiesToInsert = new List<ITableEntity>();
+            using (TextFieldParser parser = new TextFieldParser(filePath))
+            {
+                int numberOfColumns = 50;
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+
+                var headers = parser.ReadFields().Take(numberOfColumns);
+
+                while (!parser.EndOfData)
+                {
+                    try
+                    {
+                        // Uncomment this if you want to add a limit to the amount of rows to read per CSV file
+                        //if (parser.LineNumber == 1000)
+                        //{
+                        //    return entitiesToInsert;
+                        //}
+
+                        Console.WriteLine($"Processing line {parser.LineNumber}");
+                        var fields = parser.ReadFields().Take(numberOfColumns);
+                        var guid = Guid.NewGuid().ToString();
+                        var entity = new DynamicTableEntity(fields.ElementAt(14), guid);
+                        var properties = new Dictionary<string, EntityProperty>();
+                        //Processing row
+                        for (int i = 0; i < numberOfColumns; i++)
+                        {
+                            properties.Add(headers.ElementAt(i),
+                                new EntityProperty(fields.ElementAt(i)));
+                        }
+                        entity.Properties = properties;
+                        entitiesToInsert.Add(entity);
+                    }
+                    catch
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"ERROR: failed to parse line {parser.LineNumber}");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    }
+                }
+            }
+            return entitiesToInsert;
+        }
+
+        /// <summary>
+        /// Returns a List of TableBatchOpertions for row insertion
+        /// </summary>
+        /// <param name="group">All entities in this group must have the same PartitionKey</param>
+        /// <returns></returns>
+        private List<TableBatchOperation> GetTableBatchOperations(IGrouping<string, ITableEntity> group)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Inserting entities for batch {group.Key}");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            var operationsList = new List<TableBatchOperation>();
+            var entities = group.ToList();
+            var chunkSize = 100;       
+
+            while(entities.Any())
+            {
+                TableBatchOperation batchOperation = new TableBatchOperation();
+                var chunk = entities.Take(chunkSize).ToList();
+                foreach (ITableEntity entity in chunk)
+                {
+                    try
+                    {
+                        batchOperation.Insert(entity);
+                    }
+                    catch { }
+                }
+                operationsList.Add(batchOperation);
+                entities = entities.Skip(chunkSize).ToList();
+            }
+            return operationsList;
+        }
+
+        /// <summary>
+        /// Executes the supplied batch operations against the table
+        /// </summary>
+        /// <param name="tableClient"></param>
+        /// <param name="batches"></param>
+        /// <returns></returns>
+        private async Task InsertBatchOperationsAsync(CloudTableClient tableClient, IList<TableBatchOperation> batches)
+        {
+            var counter = 0;
+
+            CloudTable table = tableClient.GetTableReference("flights");
+            table.CreateIfNotExists();
+
+            foreach(var batch in batches)
+            {
+                try
+                {
+                    counter += batch.Count;
+                    Console.WriteLine($"Batches counter: {counter}");
+
+                    Stopwatch watch = new Stopwatch();
+                    watch.Start();
+                    var task = table.ExecuteBatchAsync(batch);
+                    await task;
+                    watch.Stop();
+                    Console.WriteLine($"{DateTime.UtcNow.ToString("yyyy-MM-ddThh:mm:ssK")}, {watch.ElapsedMilliseconds.ToString()}, {batch.Count}");
+                    watch.Reset();
+                }
+                catch (StorageException storageException)
+                {
+                    switch (storageException.RequestInformation.HttpStatusCode)
+                    {
+                        case 409:
+                            // Entity already exists, ignore
+                            break;
+                        case 429:
+                            System.Threading.Thread.Sleep(1000);
+                            await table.ExecuteBatchAsync(batch);
+                            break;
+                        default:
+                            throw storageException;
+                    }
+                }
+                catch(Exception e)
+                {
+                    throw e;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Main method that gets the insert operation going
+        /// </summary>
+        /// <param name="tableClient"></param>
+        /// <param name="folderPath"></param>
+        /// <returns></returns>
+        public async Task Run(CloudTableClient tableClient, string folderPath)
+        {
+            var filesToImport = GetFiles(folderPath);
+            var groupedList = new List<IGrouping<string, ITableEntity>>();
+            var batchInsertOperations = new List<TableBatchOperation>();
+
+            foreach (string filePath in filesToImport)
+            {
+
+                batchInsertOperations = new List<TableBatchOperation>();
+                groupedList = new List<IGrouping<string, ITableEntity>>();
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Uploaded from {filePath}");
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+                groupedList = ReadCsvFile(filePath).GroupBy(o => o.PartitionKey).ToList();
+                foreach(IGrouping<string, ITableEntity> group in groupedList)
+                {
+                    batchInsertOperations.AddRange(GetTableBatchOperations(group));
+                }
+                await InsertBatchOperationsAsync(tableClient, batchInsertOperations);
+            }
+
+            Console.WriteLine("Press any key to end...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# Getting started with Azure Cosmos DB: Table API
+[Azure Cosmos DB](http://cosmosdb.com) is a globally distributed, multi-model database for mission critical applications. Azure Cosmos DB provides the [Table API](https://docs.microsoft.com/azure/cosmosdb/table-introduction.md) for applications that need a key-value store with a schema-less design. Azure Cosmos DB supports throughput-optimized tables (informally called "premium tables"), currently in public preview.
+
+## About this sample: CRUD and Query using the Azure Table storage preview SDK
+
+During the preview, Azure Cosmos DB supports the Table API using the .NET SDK. You can download the [Azure Storage Preview SDK from Nuget](https://www.nuget.org/packages/WindowsAzure.Storage/8.2.0-preview), that has the same classes and method signatures as the public Azure storage SDK, but also has the ability to connect to Azure Cosmos DB accounts using the Table API.
+
+![Premium Table with Azure Cosmos DB](premium-tables.png) 
+
+This sample is for developers who are familiar with the Azure Table storage SDK, and would like to use the premium features available using Azure Cosmos DB. It is based on Get Started with Azure Table storage using .NET and shows how to take advantage of additional capabilities like secondary indexes, provisioned throughput, and multi-homing. 
+
+* Open in Visual Studio
+* Update the connection string to your Azure Cosmos DB account endpoint and keys, e.g. `<add key="StorageConnectionString" value="DefaultEndpointsProtocol=https;AccountName=account-name;AccountKey=account-key;EndpointSuffix=core.windows.net" />`
+* Run the application
+
+We provide a walkthrough of the sample at [Get started with Azure Cosmos DB's Table API using .NET](https://docs.microsoft.com/azure/cosmosdb/create-table-dotnet.md)
+
+## Premium tables overview
+If you currently use Azure Table storage, you gain the following benefits with the preview:
+
+* Turn-key global distribution with multi-homing and automatic and manual failvoers
+* Support for automatic schema-agnostic indexing against all properties ("secondary indexes"), and fast queries
+* Support for independent scaling of storage and throughput, across any number of regions
+* Support for dedicated throughput per table that can be scaled from 100s to millions of requests per second
+* Support for five tunable consistency levels to trade off availability, latency, and consistency based on your application needs
+* 99.99% availability within a single region, and ability to add more regions for higher availability, and industry-leading comprehensive SLAs on general availability
+* Work with the existing Azure storage .NET SDK, and no code changes to your application
+
